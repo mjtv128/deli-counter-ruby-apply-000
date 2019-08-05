da0939d0f102631d1a14edcfb7fe20043fa0761d
jocular-source-7570 <mjtvan@gmail.com>
2019-08-05 15:39:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,6 +14,13 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end 
 
+def take_a_number(katz_deli)
+  katz_deli.push()
+  i = 0 
+  i += 1 
+  
+end 
+
 def now_serving(x)
   if x.empty?
     puts "There is nobody waiting to be served!"
